9df8e0d1976eaaaa75c4fc74d197b5fe445076a4
jdmedlock <jdmedlock@gmail.com>
2021-10-25 11:20:35 -0500

    Feature: Add second bar for example [WIP]

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // This is based on work by Eric Winton (https://codepen.io/ericwinton/pen/YQmayz).
 // This implementation reverses the order of the generated pattern
 // so it progresses from dark to light in a left-to-right direction.
-const generateBar = (pixelatedBar, noRows, bgColor) => {	
+const generateBar = (pixelatedBar, noRows, bgColor) => {
+  
+  console.log('pixelatedBar: ', pixelatedBar)
 
   let windowWidth = window.innerWidth
   let pW = 15
@@ -17,7 +19,7 @@ const generateBar = (pixelatedBar, noRows, bgColor) => {
         var randomNumber = (Math.random() * tenth) + (tenth - .05)
       }
       const opacity = 1.7 - randomNumber.toFixed(2)
-      const pixel = document.createElement("div")
+      let pixel = document.createElement("div")
       pixel.setAttribute("style", `background-color: ${ bgColor }; opacity: ${ opacity }; height: ${ pH }px; width: ${ pW }px;`)
       pixel.setAttribute("class", "pixel")
       pixelatedBar.appendChild(pixel)
@@ -25,12 +27,20 @@ const generateBar = (pixelatedBar, noRows, bgColor) => {
   }
 }
 
-const addTextToBar = (sectionText) => {
-  let title = document.getElementById('title')
+const addTextToBar = (title, sectionText) => {
   title.innerHTML += sectionText
 }
 
-const pixels = document.querySelector("#pixels")
+window.addEventListener('load', (event) => {
+  const pixels1 = document.getElementById('pixels1')
+  console.log('pixels1: ', pixels1)
+  generateBar(pixels1, 5, "#3CB371")
+  const title1 = document.getElementById("title1")
+  addTextToBar(title1, 'Building real projects to get out of tutorial purgatory')
 
-generateBar(pixels, 5, "#3CB371")
-addTextToBar('"Building real projects to get out of tutorial purgatory"')
+  const pixels2 = document.getElementById('pixels2')
+  console.log('pixels2: ', pixels2)
+  generateBar(pixels2, 5, "#7D498D")
+  const title2 = document.getElementById("title2")
+  addTextToBar(title2, 'Chingu helped me land my dream job!')
+})
