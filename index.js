8ef5fd6a417f99f65fdd666499ce870024320e11
Jim Medlock <jdmedlock@gmail.com>
2021-10-28 20:32:40 -0500

    Feature: Center section text in cross axis (vertical)

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // This is based on work by Eric Winton (https://codepen.io/ericwinton/pen/YQmayz).
 // This implementation reverses the order of the generated pattern
 // so it progresses from dark to light in a left-to-right direction.
+const pixelDimensions = 15
 const generateBar = (pixelatedBar, noRows, bgColor) => {
   let windowWidth = window.innerWidth
-  let pW = 15
+  let pW = pixelDimensions
   let pH = pW
   let noColumns = parseInt(windowWidth/pW)
 
   for (let r = 0; r < noRows; r++) {
     for (let c = 0; c < noColumns; c++) {
+      let randomNumber
       const tenth = c/noColumns
       if (c < noColumns/10) {
-        var randomNumber = (Math.random() * tenth)
+        randomNumber = (Math.random() * tenth)
       } else {
-        var randomNumber = (Math.random() * tenth) + (tenth - .05)
+        randomNumber = (Math.random() * tenth) + (tenth - .05)
       }
       const opacity = 1.7 - randomNumber.toFixed(2)
       let pixel = document.createElement("div")
@@ -30,17 +32,13 @@ window.addEventListener('load', (event) => {
     "#F5521F",
     "#4362AB",
     "#3CB371",
-    "#F5521F",
-    "#4362AB",
-    "#3CB371",
-    "#F5521F",
-    "#4362AB",
-    "#3CB371",
-    "#F5521F",
-    "#4362AB",
   ]
   const pixelBars = document.getElementsByClassName('pixels')
   for (i = 0; i < pixelBars.length; i++) {
-    generateBar(pixelBars[i], 5, barColors[i])
+    const sectionId = 'section'.concat((i+1).toString())
+    const sectionBar = document.getElementById(sectionId)
+    const barRows = sectionBar.getAttribute('barRows')
+    sectionBar.setAttribute("style", `height: ${ pixelDimensions*barRows }px;`)
+    generateBar(pixelBars[i], barRows, barColors[i])
   }
 })
